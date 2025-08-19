0ea321763fe227e28ac74e5b20f2914b9a2cfbc0
Maxim Toropygin <mrcatman1337@gmail.com>
2025-08-19 20:57:10 +0300

    config init

--- gw/cmd/gw/main.go
+++ gw/cmd/gw/main.go
@@ -1,9 +1,25 @@
 package main
 
-import "github.com/gin-gonic/gin"
+import (
+	"github.com/gin-gonic/gin"
+	"github.com/spf13/viper"
+)
 
 func main() {
+	configure()
+
 	g := gin.Default()
 
-	g.Run()
+	gwPort := ":" + viper.GetString("gateway.port")
+	g.Run(gwPort)
+}
+
+func configure() {
+	viper.SetConfigName("config")
+	viper.SetConfigType("yaml")
+	viper.AddConfigPath("configs")
+	err := viper.ReadInConfig()
+	if err != nil {
+		panic(err)
+	}
 }
