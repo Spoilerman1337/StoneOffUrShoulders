30d67eca1cb7d8010da0fe9567ec70a4b13047fb
Maxim Toropygin <mrcatman1337@gmail.com>
2025-09-07 10:57:28 +0300

    sliding window (log)

--- SOUS.Gateway/internal/rate_limiter/fixed_window.go
+++ SOUS.Gateway/internal/rate_limiter/fixed_window.go
@@ -7,12 +7,12 @@ import (
 )
 
 type FixedWindowRateLimiter struct {
-	storage map[string]*requestData
+	storage map[string]*fixedWindowRequestData
 	config  shared.RateLimiterConfig
 	mutex   sync.Mutex
 }
 
-type requestData struct {
+type fixedWindowRequestData struct {
 	count    int
 	earliest time.Time
 }
@@ -22,7 +22,7 @@ func (rl *FixedWindowRateLimiter) IsAllowed(key string) bool {
 	defer rl.mutex.Unlock()
 
 	now := time.Now()
-	window := time.Duration(rl.config.Rate)
+	window := time.Duration(rl.config.Rate) * time.Millisecond
 
 	data, exists := rl.storage[key]
 
@@ -31,9 +31,31 @@ func (rl *FixedWindowRateLimiter) IsAllowed(key string) bool {
 	}
 
 	if !exists || now.After(data.earliest.Add(window)) {
-		rl.storage[key] = &requestData{count: 0, earliest: now}
+		rl.storage[key] = &fixedWindowRequestData{count: 0, earliest: now}
 	}
 
 	rl.storage[key].count++
 	return true
+}
+
+// Cleanup TODO: Decouple storage from implementation, make it storage agnostic, with option of using smth like Redis.
+// Cleanup won't be needed in Redis implementation, it has its own TTL mechanism
+func (rl *FixedWindowRateLimiter) Cleanup(interval time.Duration) {
+	go func() {
+		ticker := time.NewTicker(interval)
+		defer ticker.Stop()
+
+		for range ticker.C {
+			rl.mutex.Lock()
+			now := time.Now()
+			window := time.Duration(rl.config.Rate) * time.Millisecond
+			for key, data := range rl.storage {
+				if now.After(data.earliest.Add(window)) {
+					delete(rl.storage, key)
+				}
+			}
+
+			rl.mutex.Unlock()
+		}
+	}()
 }
---
+++ SOUS.Gateway/internal/rate_limiter/sliding_window.go
@@ -0,0 +1,52 @@
+package rate_limiter
+
+import (
+	"gateway/internal/shared"
+	"sync"
+	"time"
+)
+
+type SlidingWindowRateLimiter struct {
+	storage map[string]*slidingWindowRequestData
+	config  shared.RateLimiterConfig
+	mutex   sync.Mutex
+}
+
+type slidingWindowRequestData struct {
+	timestamps []time.Time
+}
+
+func (rl *SlidingWindowRateLimiter) IsAllowed(key string) bool {
+	rl.mutex.Lock()
+	defer rl.mutex.Unlock()
+
+	now := time.Now()
+
+	data, exists := rl.storage[key]
+
+	if !exists {
+		rl.storage[key] = &slidingWindowRequestData{}
+	} else {
+		rl.cleanMap(data, now)
+	}
+
+	if len(rl.storage[key].timestamps) >= rl.config.Limit {
+		return false
+	}
+
+	data.timestamps = append(data.timestamps, now)
+	return true
+}
+
+func (rl *SlidingWindowRateLimiter) cleanMap(storage *slidingWindowRequestData, now time.Time) {
+	window := time.Duration(rl.config.Rate) * time.Millisecond
+
+	newSlice := storage.timestamps[:0]
+	for _, timestamp := range storage.timestamps {
+		if timestamp.After(now.Add(-window)) {
+			newSlice = append(newSlice, timestamp)
+		}
+	}
+
+	storage.timestamps = newSlice
+}
